c0b57a49781ed906f2186f9ca74fe5d904fe4576
naybala <naybalaismylife.nbl@gmail.com>
2022-07-01 12:54:02 +0630

    commit

--- index.php
+++ index.php
@@ -14,10 +14,28 @@
 
     <!-- styles -->
     <link rel="stylesheet" href="Ui/asset/main-ui/styles.css" />
+    <style>
+    .footerSvgOne {
+        position: absolute;
+        top: auto;
+        left: 0;
+        width: 15rem;
+        z-index: 1;
+    }
 
+    .footerSvgTwo {
+        position: absolute;
+        top: auto;
+        right: 0;
+        width: 15rem;
+        z-index: 1;
+    }
+    </style>
 </head>
 
 <body id="home">
+
+    <!--Nav Section Start -->
     <nav class="navbar navbar-expand-lg py-2 fixed-top bg-dark ">
         <div class="container">
             <h1 class="text-light">Hello</h1>
@@ -46,11 +64,13 @@
             </div>
         </div>
     </nav>
-    <br><br> <br>
+
+    <!--Nav Section End -->
 
 
     <!-- Show Case Section Start -->
     <section class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start" id="home">
+        <br><br> <br>
         <div class="container">
             <div class="d-sm-flex align-items-center justify-content-between">
                 <img class="img-fluid w-50 d-none d-sm-block" src="./Ui/image/showcase.svg" alt="" />
@@ -179,10 +199,12 @@
             </div>
         </div>
     </section>
+    <br><br>
     <!-- Box Section End -->
 
     <!-- Question Section Start -->
     <section id="questions" class="p-5">
+        <br><br>
         <div class="container">
             <h2 class="text-center mb-4">Frequently Asked Questions</h2>
             <div class="accordion accordion-flush" id="questions">
@@ -325,7 +347,78 @@
     </section>
     <!-- Question Section Start -->
 
+    <!-- Footer Start-->
+    <footer class="p-4 bg-dark text-white text-center position-relative footer">
+        <p class="lead">Copyright &copy; 2022</p>
+        <section class="p-5">
+            <div class="container">
+                <div class="row text-center g-4 g-sm-0">
+                    <div class="footerSvgOne">
+                        <img class="img-fluid d-none d-sm-block" src="./Ui/image/happy_news.svg" alt="" />
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card bg-dark text-light">
+                            <div class="card-body text-center">
+                                <div class="h1 mb-3">
+                                    <i class="bi bi-telephone-outbound-fill"></i>
+                                </div>
+                                <h3 class="card-title mb-3">Virtual</h3>
+                                <p class="card-text">
+                                    <span class="text-primary">Hot-line</span> : 09763684400 <br />
+                                    <span class="text-primary">Office-ph</span> : 09763684400 <br />
+                                    <span class="text-primary">Current-line</span> : 09763684400 <br />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card bg-dark text-light">
+                            <div class="card-body text-center">
+                                <div class="h1 mb-3">
+                                    <i class="bi bi-envelope-open-fill"></i>
+                                </div>
+                                <h3 class="card-title mb-3">Email</h3>
+                                <p class="card-text">
+                                    <span class="text-primary">Email</span> : adipisicielitIlloisi@email.com
+                                    <br />
+                                    <span class="text-primary">Gmail</span>
+                                    :adipisicielitIlloisi@gmail.com <br />
+                                    <span class="text-primary">Microsoft Mail</span>
+                                    :adipisicielitIlloisi@outlook.com <br />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="card bg-dark text-light">
+                            <div class="card-body text-center">
+                                <div class="h1 mb-3">
+                                    <i class="bi bi-people"></i>
+                                </div>
+                                <h3 class="card-title mb-3">In Person</h3>
+                                <p class="card-text">
+                                    <span class="text-primary">Guy One</span> :
+                                    adipisicielitIlloisi@email.com <br />
+                                    <span class="text-primary">Guy Two</span>
+                                    :adipisicielitIlloisi@gmail.com <br />
+                                    <span class="text-primary">Guy Three</span>
+                                    :adipisicielitIlloisi@outlook.com <br />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footerSvgTwo">
+                        <img class="img-fluid d-none d-sm-block" src="./Ui/image/certificate.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
 
+        <!-- <a href="#" class="position-absolute bottom-0 end-0 p-5">
+        <i class="bi bi-arrow-up-circle h1"></i>
+      </a> -->
+    </footer>
+    <!-- Footer End-->
 
     <script src="Ui/asset//main-ui/app.js"></script>
 </body>
